05bd811d1724b50b9d90fa5fb7392c63259bcf65
IgorFilin <filinigor@yandex.ru>
2023-12-31 00:13:23 +0300

    Логика передвижения, прыжков

--- logic.js
+++ logic.js
@@ -17,7 +17,10 @@ class Player {
       height: 50,
     };
     this.gravity = 0.5;
-    this.speed = 0;
+    this.speed = {
+      x: 0,
+      y: 0,
+    };
   }
   create() {
     ctx.fillRect(
@@ -28,10 +31,25 @@ class Player {
     );
   }
   moveGravity() {
-    if (this.position.y + this.size.height <= canvas.height) {
-      this.position.y += this.speed;
-      this.speed += this.gravity;
-    } else this.speed = 0;
+    this.position.y += this.speed.y;
+    this.position.x += this.speed.x;
+    if (this.position.y + this.size.height < canvas.height) {
+      this.speed.y += this.gravity;
+    } else this.speed.y = 0;
+  }
+  move(keyCode) {
+    switch (keyCode) {
+      case 39:
+        this.speed.x += 7;
+        break;
+      case 37:
+        this.speed.x -= 7;
+        break;
+      case 38: {
+        this.speed.y -= 15;
+        break;
+      }
+    }
   }
   render() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -41,12 +59,17 @@ class Player {
   }
 }
 
+// Слушатели событий
+
+addEventListener("keydown", (e) => {
+  // 39 right, 37 left, 38 up
+  player.move(e.keyCode);
+});
+
+addEventListener("keyup", () => {
+  player.speed.x = 0;
+});
+
 const player = new Player();
-
-// function render() {
-//   player.render();
-
-//   console.log("render");
-// }
 
 player.render();
